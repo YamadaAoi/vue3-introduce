5d5a9e382f1490e061714e6868f9010a006850b7
zhouyinkui <zhouyinkui@mlogcn.com>
2022-03-21 00:53:21 +0800

    code

---
+++ src/docs/vue2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'VueTwo',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  created() {
+    console.log('created')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/docs/vue3.vue
@@ -0,0 +1,42 @@
+<template>
+  <div></div>
+  <div></div>
+</template>
+
+<script>
+import { onMounted, reactive, watch } from 'vue'
+
+export default {
+  name: 'VueThree',
+  props: {},
+  setup(props) {
+    const state = reactive({})
+
+    onMounted(() => {
+      // mounted
+    })
+
+    watch(
+      () => props,
+      (newProps, preProps) => {
+        console.log(newProps, preProps)
+      },
+      {
+        deep: true,
+        immediate: true
+      }
+    )
+
+    const getList = () => {
+      // methods
+    }
+
+    return {
+      state,
+      getList
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
